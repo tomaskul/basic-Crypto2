f3adac357b4dc1ac04f5908d611deee73f3417de
Tomas <psytk2@nottingham.ac.uk>
2018-04-21 22:00:43 +0100

    Adding few unit tests

---
+++ SimpleCryptographyUnitTests/Cipher Tests/Caesar Shift Cipher/CaesarShiftCipherTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SimpleCryptography.Ciphers.Caesar_Shift_Cipher;
+
+namespace SimpleCryptographyUnitTests.Cipher_Tests.Caesar_Shift_Cipher
+{
+    [TestFixture]
+    public class CaesarShiftCipherTests
+    {
+        private static readonly CaesarShiftCipher Cipher = new CaesarShiftCipher();
+        private const string EnglishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string EngAlphabetMissingLetters = "ABCDFGHJKLMNOPRTUVWXYZ"; //I, E, Q, S missing
+        
+        [Test]
+        public void Encrypt_Shift_5_CorrectAlphabet()
+        {
+            var encryptedMessage = Cipher.EncryptMessage("Hi Dave!", EnglishAlphabet, 5);
+            
+            Assert.AreEqual("MN IFAJ!", encryptedMessage);
+        }
+
+        [Test]
+        public void Decrypt_Shift_7_CorrectAlphabet()
+        {
+            var decryptedMessage = Cipher.DecryptMessage("JHUJLS AOL MSPNOA", EnglishAlphabet, 7);
+            
+            Assert.AreEqual("cancel the flight", decryptedMessage);
+        }
+
+        [Test]
+        public void Encrypt_Shift_4_BrokenKey()
+        {
+            var encryptedMessage = Cipher.EncryptMessage("I missed second question", EngAlphabetMissingLetters, 4);
+            
+            Assert.AreEqual("I RISSEJ SEHUTJ QYESXIUT", encryptedMessage);
+        }
+    }
+}
---
+++ SimpleCryptographyUnitTests/Cipher Tests/Monoalphabetic Substitution Cipher/MonoalphabeticSubstitutionCipherTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleCryptography.Ciphers.Monoalphabetic_Substitution_Cipher;
+
+namespace SimpleCryptographyUnitTests.Cipher_Tests.Monoalphabetic_Substitution_Cipher
+{
+    [TestFixture]
+    public class MonoalphabeticSubstitutionCipherTests
+    {
+        private static readonly MonoalphabeticSubstitutionCipher Cipher = new MonoalphabeticSubstitutionCipher();
+        private static readonly MonoalphabeticSubstitutionKey Key = new MonoalphabeticSubstitutionKey(new Dictionary<char, char>()
+        {
+            { 'm', 'A' }, { 'g', 'B' }, { 'a', 'C' }, { 'r', 'D' }, { 'b', 'E' },
+            { 'q', 'F' }, { 'w', 'G' }, { 'v', 'H' }, { 'n', 'I' }, { 'l', 'J' },
+            { 'o', 'K' }, { 'p', 'L' }, { 'h', 'M' }, { 'j', 'N' }, { 'f', 'O' },
+            { 'x', 'P' }, { 'c', 'Q' }, { 'd', 'R' }, { 'e', 'S' }, { 'i', 'T' },
+            { 'k', 'U' }, { 'y', 'V' }, { 's', 'W' }, { 't', 'X' }, { 'u', 'Y' },
+            { 'z', 'Z' }, { ' ', ' ' }, { '.', '.' }, { '\'','\'' }, { ',', ',' },
+            { ';', ';' }, { '!', '!' }
+        });
+        private static readonly MonoalphabeticSubstitutionKey KeyNoPunctuation = new MonoalphabeticSubstitutionKey(new Dictionary<char, char>()
+        {
+            { 'm', 'A'}, { 'g', 'B'}, { 'a', 'C'}, { 'r', 'D'}, { 'b', 'E'},
+            { 'q', 'F'}, { 'w', 'G'}, { 'v', 'H'}, { 'n', 'I'}, { 'l', 'J'},
+            { 'o', 'K'}, { 'p', 'L'}, { 'h', 'M'}, { 'j', 'N'}, { 'f', 'O'},
+            { 'x', 'P'}, { 'c', 'Q'}, { 'd', 'R'}, { 'e', 'S'}, { 'i', 'T'},
+            { 'k', 'U'}, { 'y', 'V'}, { 's', 'W'}, { 't', 'X'}, { 'u', 'Y'},
+            { 'z', 'Z'}
+        });
+
+        [Test]
+        public void Encrypt()
+        {
+            var encryptedMessage = Cipher.EncryptMessage("Hi Dave!", Key);
+            
+            Assert.AreEqual("MT RCHS!", encryptedMessage);
+        }
+
+        [Test]
+        public void Encrypt_NoPunctuation()
+        {
+            var encryptedMessage = Cipher.EncryptMessage("Hi Dave!", KeyNoPunctuation);
+            
+            Assert.AreEqual("MTRCHS", encryptedMessage);
+        }
+
+        [Test]
+        public void Decrypt()
+        {
+            var decryptedMessage = Cipher.DecryptMessage("RKI'X LYEJTWM XMS LCWWGKDR!", Key);
+            
+            Assert.AreEqual("don't publish the password!", decryptedMessage);
+        }
+
+        [Test]
+        public void DecryptNoPunctuation()
+        {
+            var decryptedMessage = Cipher.DecryptMessage("RKIXLYEJTWMXMSLCWWGKDR", KeyNoPunctuation);
+            
+            Assert.AreEqual("dontpublishthepassword", decryptedMessage);
+        }
+    }
+}
