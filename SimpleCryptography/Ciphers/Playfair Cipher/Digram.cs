a44235687d2b3e674e69cb4fb1c95f1de1d6c690
Tomas <psytk2@nottingham.ac.uk>
2018-06-18 01:35:28 +0100

    Adding digram data object

---
+++ SimpleCryptography/Ciphers/Playfair Cipher/Digram.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleCryptography.Ciphers.Playfair_Cipher
+{
+    public class Digram
+    {
+        /// <summary>
+        /// First character within the digram.
+        /// </summary>
+        public char? CharacterOne { get; set; } = null;
+
+        /// <summary>
+        /// Second character within the digram.
+        /// </summary>
+        public char? CharacterTwo { get; set; } = null;
+
+        /// <summary>
+        /// Determines whether the digram is complete (i.e. both characters set)
+        /// </summary>
+        /// <returns></returns>
+        public bool IsComplete()
+        {
+            return CharacterOne != null && CharacterTwo != null;
+        }
+    }
+}
