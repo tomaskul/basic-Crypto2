1a7adaa63bda9201d27c0d49ca73bb206e4d4db2
Tomas <psytk2@nottingham.ac.uk>
2018-09-21 12:30:01 +0100

    Removing redundant properties from AnalyzedCharacter

--- SimpleCryptography/CipherCrackers/FrequencyAnalysis/AnalyzedCharacter.cs
+++ SimpleCryptography/CipherCrackers/FrequencyAnalysis/AnalyzedCharacter.cs
@@ -1,29 +1,13 @@
 ﻿namespace SimpleCryptography.CipherCrackers.FrequencyAnalysis
 {
-    public class AnalyzedCharacter
+    public class AnalyzedCharacter : AnalysedEntity
     {
         /// <summary>
         /// Character within source text.
         /// </summary>
         public char Character { get; set; }
         
-        /// <summary>
-        /// Number of times the character appears within source.
-        /// </summary>
-        public int OccurenceCount { get; set; }
-        
-        /// <summary>
-        /// Percentage of the text that the character makes up.
-        /// </summary>
-        public decimal Frequency { get; set; }
-
-        public AnalyzedCharacter()
-        {
-            OccurenceCount = 0;
-            Frequency = 0.0m;
-        }
-
-        public AnalyzedCharacter(char character) : this()
+        public AnalyzedCharacter(char character)
         {
             Character = character;
         }
