43134f24ed95236d4b60e5cca5069c25ed8f1703
Tomas <psytk2@nottingham.ac.uk>
2018-09-21 21:56:53 +0100

    Renaming /Digrams sub-directory to /Digraphs

---
+++ SimpleCryptography/Ciphers/Playfair Cipher/Digraths/Digraph.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleCryptography.Ciphers.Playfair_Cipher.Digraths
+{
+    public class Digraph
+    {
+        /// <summary>
+        /// First character within the digrath.
+        /// </summary>
+        public char CharacterOne { get; private set; }
+
+        /// <summary>
+        /// Second character within the digrath.
+        /// </summary>
+        public char CharacterTwo { get; private set; }
+
+        public Digraph(char characterOne, char characterTwo)
+        {
+            CharacterOne = characterOne;
+            CharacterTwo = characterTwo;
+        }
+
+        public override string ToString()
+        {
+            return $"'{CharacterOne}{CharacterTwo}'"; 
+        }
+    }
+}
---
+++ SimpleCryptography/Ciphers/Playfair Cipher/Digraths/NullableDigrath.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleCryptography.Ciphers.Playfair_Cipher.Digraths
+{
+    public class NullableDigrath
+    {
+        /// <summary>
+        /// First character within the digrath.
+        /// </summary>
+        public char? CharacterOne { get; set; } = null;
+
+        /// <summary>
+        /// Second character within the digrath.
+        /// </summary>
+        public char? CharacterTwo { get; set; } = null;
+
+        public override string ToString()
+        {
+            if (IsComplete()) { return $"'{CharacterOne}{CharacterTwo}'"; }
+
+            return CharacterOne != null ? $"'{CharacterOne}_'" : $"'_{CharacterTwo}'";
+        }
+        
+        private bool IsComplete()
+        {
+            return CharacterOne != null && CharacterTwo != null;
+        }
+    }
+}
