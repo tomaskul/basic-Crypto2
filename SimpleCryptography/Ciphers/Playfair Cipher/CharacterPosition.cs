98479ff57a10e963a9a837fe1d23db06704d2c9c
Tomas <psytk2@nottingham.ac.uk>
2018-09-21 22:47:09 +0100

    Adding property summaries to CharacterPosition

--- SimpleCryptography/Ciphers/Playfair Cipher/CharacterPosition.cs
+++ SimpleCryptography/Ciphers/Playfair Cipher/CharacterPosition.cs
@@ -2,8 +2,19 @@
 {
     public class CharacterPosition
     {
+        /// <summary>
+        /// Character being processed.
+        /// </summary>
         public char Character { get; private set; }
+        
+        /// <summary>
+        /// Character row value.
+        /// </summary>
         public int? Row { get; set; } = null;
+        
+        /// <summary>
+        /// Character column value.
+        /// </summary>
         public int? Column { get; set; } = null;
 
         public CharacterPosition(char character)
