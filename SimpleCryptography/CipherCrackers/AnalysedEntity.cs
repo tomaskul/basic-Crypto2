3cb3c66e7af71f03f57854637eed73554cc0c531
Tomas <psytk2@nottingham.ac.uk>
2018-09-21 12:29:34 +0100

    Adding AnalysedEntity to act as base class

---
+++ SimpleCryptography/CipherCrackers/AnalysedEntity.cs
@@ -0,0 +1,23 @@
+﻿using SimpleCryptography.CipherCrackers.FrequencyAnalysis;
+
+namespace SimpleCryptography.CipherCrackers
+{
+    public abstract class AnalysedEntity
+    {
+        /// <summary>
+        /// Number of times the entity appears within the source.
+        /// </summary>
+        public int OccurenceCount { get; set; }
+        
+        /// <summary>
+        /// Percentage of the text that the character makes up.
+        /// </summary>
+        public decimal Frequency { get; set; }
+
+        protected AnalysedEntity()
+        {
+            OccurenceCount = 0;
+            Frequency = 0.0m;
+        }
+    }
+}
