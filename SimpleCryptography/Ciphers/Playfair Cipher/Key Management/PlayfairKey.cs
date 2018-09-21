aa692d65fdc1e14717885c06f0bf576c45c02510
Tomas <psytk2@nottingham.ac.uk>
2018-09-22 00:35:54 +0100

    Adding dedicated Playfair Cipher Key object in hopes to simplify the cipher implementation

---
+++ SimpleCryptography/Ciphers/Playfair Cipher/Key Management/PlayfairKey.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleCryptography.Ciphers.Playfair_Cipher.Key_Management
+{   
+    /// <summary>
+    /// Playfair cipher key.
+    /// </summary>
+    public class PlayfairKey
+    {
+        /// <summary>
+        /// Playfair cipher key value.
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
