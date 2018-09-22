f5673d63ef93d9c66464652cd87cc8246e497829
Tomas <psytk2@nottingham.ac.uk>
2018-09-22 20:07:00 +0100

    Redoing the implementation of the encryption/decryption logic. Instead of using 2D char array, a simple string will be used. This makes validation functions easier to do and allows to break down the implementation down into smaller chunks which allow for more straight forward decryption function implementation.

--- SimpleCryptography/Ciphers/Playfair Cipher/IPlayfairCipher.cs
+++ SimpleCryptography/Ciphers/Playfair Cipher/IPlayfairCipher.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleCryptography.Ciphers.Playfair_Cipher
+﻿using SimpleCryptography.Ciphers.Playfair_Cipher.Key_Management;
+
+namespace SimpleCryptography.Ciphers.Playfair_Cipher
 {
     /// <summary>
     /// Interface for Playfair cipher. Read more here: https://en.wikipedia.org/wiki/Playfair_cipher
@@ -9,32 +11,32 @@
         /// Encrypts a plain text message by using the specified key.
         /// </summary>
         /// <param name="plainText">Text to encrypt.</param>
-        /// <param name="key">Key to encrypt text with.</param>
+        /// <param name="memorableKey">Memorable key to encrypt text with.</param>
         /// <returns>Encrypted plaintext</returns>
-        string EncryptMessage(string plainText, string key);
+        string EncryptMessage(string plainText, string memorableKey);
 
         /// <summary>
         /// Encrypts a plain text message by using a cipher key.
         /// </summary>
         /// <param name="plainText">Text to encrypt.</param>
-        /// <param name="cipherKey">2D playfair cipher key.</param>
+        /// <param name="cipherKey">Playfair cipher key.</param>
         /// <returns>Encrypted plaintext</returns>
-        string EncryptMessage(string plainText, char[,] cipherKey);
+        string EncryptMessage(string plainText, PlayfairKey cipherKey);
         
         /// <summary>
         /// Decrypts ciphertext message by using the specified key.
         /// </summary>
         /// <param name="cipherText">Plaintext encrypted via playfair key.</param>
-        /// <param name="key">Key to use for decryption.</param>
+        /// <param name="memorableKey">Key to use for decryption.</param>
         /// <returns>Plaintext derived by decrypting ciphertext via supplied key.</returns>
-        string DecryptMessage(string cipherText, string key);
+        string DecryptMessage(string cipherText, string memorableKey);
 
         /// <summary>
         /// Decypts a ciphertext message by using the specified cipher key.
         /// </summary>
         /// <param name="cipherText">Plaintext encrypted via playfair key.</param>
-        /// <param name="cipherKey">2D playfair cipher key.</param>
+        /// <param name="cipherKey">Playfair cipher key.</param>
         /// <returns>Plaintext derived by decrypting ciphertext via supplied key.</returns>
-        string DecryptMessage(string cipherText, char[,] cipherKey);
+        string DecryptMessage(string cipherText, PlayfairKey cipherKey);
     }
 }
--- SimpleCryptography/Ciphers/Playfair Cipher/PlayfairCipher.cs
+++ SimpleCryptography/Ciphers/Playfair Cipher/PlayfairCipher.cs
@@ -25,114 +25,239 @@ namespace SimpleCryptography.Ciphers.Playfair_Cipher
             _digrathGenerator = digrathGenerator;
             _keyManagement = playfairKeyManagement;
         }
-        
-        public string EncryptMessage(string plainText, string key)
+
+        public string EncryptMessage(string plainText, string memorableKey)
         {
-            var cipherKey = _keyManagement.GenerateCipherKey(key);
+            var cipherKey = _keyManagement.GenerateCipherKey(memorableKey);
             var sanitizedMessage = PlayfairUtil.GetSanitisedString(plainText);
             var digraphs = _digrathGenerator.GetMessageDigraths(sanitizedMessage);
 
             return Encrypt(digraphs, cipherKey);
         }
 
-        public string EncryptMessage(string plainText, char[,] cipherKey)
+        public string EncryptMessage(string plainText, PlayfairKey cipherKey)
         {
-            if (!_keyManagement.IsValidCipherKey(cipherKey)) { throw new ArgumentException("Invalid cipher key."); }
+            if (!_keyManagement.IsValidCipherKey(cipherKey))
+            {
+                throw new ArgumentException("Invalid cipher key.");
+            }
+
             var sanitizedMessage = PlayfairUtil.GetSanitisedString(plainText);
             var digraths = _digrathGenerator.GetMessageDigraths(sanitizedMessage);
 
             return Encrypt(digraths, cipherKey);
         }
 
-        public string DecryptMessage(string cipherText, string key)
+        public string DecryptMessage(string cipherText, string memorableKey)
         {
-            throw new System.NotImplementedException();
-        }
-        
-        public string DecryptMessage(string cipherText, char[,] cipherKey)
-        {
-            throw new NotImplementedException();
+            return Decrypt(_digrathGenerator.GetCipherTextDigraphs(cipherText),
+                _keyManagement.GenerateCipherKey(memorableKey));
         }
 
-        #region Encryption
+        public string DecryptMessage(string cipherText, PlayfairKey cipherKey)
+        {
+            if (!_keyManagement.IsValidCipherKey(cipherKey))
+            {
+                throw new ArgumentException("Invalid cipher key.");
+            }
 
-        private static string Encrypt(IEnumerable<Digraph> digraths, char[,] cipherKey)
+            return Decrypt(_digrathGenerator.GetCipherTextDigraphs(cipherText), cipherKey);
+        }
+
+        /// <summary>
+        /// Performs the encryption.
+        /// </summary>
+        /// <param name="digraths">Plain text digraphs.</param>
+        /// <param name="key">Encryption cipher key.</param>
+        /// <returns>Encrypted cipher text.</returns>
+        private static string Encrypt(IEnumerable<Digraph> digraths, PlayfairKey key)
+        {
+            return PerformGenericPlayfair(digraths, key);
+        }
+
+        /// <summary>
+        /// Performs decryption.
+        /// </summary>
+        /// <param name="digraphs">Cipher text digraphs.</param>
+        /// <param name="key">Encryption cipher key.</param>
+        /// <returns>Decrypted cipher text.</returns>
+        private static string Decrypt(IEnumerable<Digraph> digraphs, PlayfairKey key)
+        {
+            return PerformGenericPlayfair(digraphs, key, true).ToLower();
+        }
+
+        #region Generic Algorithm Functions
+
+        private static string PerformGenericPlayfair(IEnumerable<Digraph> digraphs, PlayfairKey key,
+            bool isDecrypt = false)
         {
             var sb = new StringBuilder(string.Empty);
 
-            foreach (var digraph in digraths)
+            foreach (var digraph in digraphs)
             {
-                var charOnePosition = new CharacterPosition(digraph.CharacterOne);
-                var charTwoPosition = new CharacterPosition(digraph.CharacterTwo);
-
-                for (var row = 0; row < CipherGridDimension; row++)
+                // Find digraph character index values.
+                var charOneIndex = 0;
+                var charTwoIndex = 0;
+                for (var i = 0; i < key.Value.Length; i++)
                 {
-                    for (var column = 0; column < CipherGridDimension; column++)
+                    if (key.Value[i].Equals(digraph.CharacterOne))
                     {
-                        if (cipherKey[row, column].Equals(digraph.CharacterOne))
-                        {
-                            charOnePosition.Row = row;
-                            charOnePosition.Column = column;
-                        }
-                        else if (cipherKey[row, column].Equals(digraph.CharacterTwo))
-                        {
-                            charTwoPosition.Row = row;
-                            charTwoPosition.Column = column;
-                        }
+                        charOneIndex = i;
+                    }
+                    else if (key.Value[i].Equals(digraph.CharacterTwo))
+                    {
+                        charTwoIndex = i;
                     }
                 }
 
-                // Throw exception if for some reason necessary fields weren't initialised correctly.
-                ThrowIfUninitialisedCharacterPosition(charOnePosition);
-                ThrowIfUninitialisedCharacterPosition(charTwoPosition);
-
-                if (charOnePosition.Row == charTwoPosition.Row)
+                if (AreOnTheSameRow(charOneIndex, charTwoIndex))
                 {
-                    // Same row - replace by characters immediately to the right of each char in digram.
-                    // if at the end of the grid, use initial element.
-                    
-                    sb.Append(cipherKey[charOnePosition.Row.Value,
-                        charOnePosition.Column.Value + 1 == CipherGridDimension
-                            ? 0
-                            : charOnePosition.Column.Value + 1]);
-
-                    sb.Append(cipherKey[charTwoPosition.Row.Value,
-                        charTwoPosition.Column.Value + 1 == CipherGridDimension
-                            ? 0
-                            : charTwoPosition.Column.Value + 1]);
+                    sb.Append(GetCharacterSameRow(charOneIndex, key, isDecrypt));
+                    sb.Append(GetCharacterSameRow(charTwoIndex, key, isDecrypt));
                 }
-                else if (charOnePosition.Column == charTwoPosition.Column)
+                else if (AreInTheSameColumn(charOneIndex, charTwoIndex))
                 {
-                    // Same column - replace by characters immediately below each char in digram.
-                    // If at the bottom of the grid, use top element.
-
-                    sb.Append(cipherKey[charOnePosition.Row.Value + 1 == CipherGridDimension
-                        ? 0
-                        : charOnePosition.Row.Value + 1, charOnePosition.Column.Value]);
-
-                    sb.Append(cipherKey[charTwoPosition.Row.Value + 1 == CipherGridDimension
-                        ? 0
-                        : charTwoPosition.Row.Value + 1, charTwoPosition.Column.Value]);
+                    sb.Append(GetCharacterSameColumn(charOneIndex, key, isDecrypt));
+                    sb.Append(GetCharacterSameColumn(charTwoIndex, key, isDecrypt));
                 }
                 else
                 {
-                    // Neither same row nor same column - 1st char = char1 row & char2 column,
-                    // 2nd char = char2 row & char1 column.
-                    sb.Append(cipherKey[charOnePosition.Row.Value, charTwoPosition.Column.Value]);
-                    sb.Append(cipherKey[charTwoPosition.Row.Value, charOnePosition.Column.Value]);
+                    sb.Append(GetSquareChar(charOneIndex, charTwoIndex, key, isDecrypt));
+                    sb.Append(GetSquareChar(charOneIndex, charTwoIndex, key, isDecrypt, true));
                 }
             }
 
             return sb.ToString();
         }
-        
-        private static void ThrowIfUninitialisedCharacterPosition(CharacterPosition characterPosition)
+
+        /// <summary>
+        /// Rule One: Characters are on the same row, replace by characters immediately to the right (encryption) / left
+        /// (decryption) of each character in the digrath. If at the end of the grid (encryption), use initial element.
+        /// If at the start of the grid (decryption), use last element.
+        /// </summary>
+        /// <param name="charOneIndex"></param>
+        /// <param name="charTwoIndex"></param>
+        /// <returns></returns>
+        private static bool AreOnTheSameRow(int charOneIndex, int charTwoIndex)
         {
-            if (characterPosition.Row == null || characterPosition.Column == null)
+            return GetCharRowIndex(charOneIndex) == GetCharRowIndex(charTwoIndex);
+        }
+
+        /// <summary>
+        /// Rule Two: Characters are in the same column, replace by characters immediately below (encryption) / above
+        /// (decryption) each character in the digrath. If at the bottom (encryption), use top element. If at the top
+        /// (decryption) use bottom element.
+        /// </summary>
+        /// <param name="charOneIndex"></param>
+        /// <param name="charTwoIndex"></param>
+        /// <returns></returns>
+        private static bool AreInTheSameColumn(int charOneIndex, int charTwoIndex)
+        {
+            return GetCharColumnIndex(charOneIndex) == GetCharColumnIndex(charTwoIndex);
+        }
+
+        /// <summary>
+        /// Based on the current index and whether attempting to encrypt/decrypt appropriate character is retrieved
+        /// from the cipher key value. This method applies when both of the characters are on the same row.
+        /// </summary>
+        /// <param name="currentIndex">Current index of the character attempting to encrypt/decrypt.</param>
+        /// <param name="key">Cipher key being used for encryption/decryption.</param>
+        /// <param name="isDecryption">Flag determining whether to use decryption rule set. If not set to <c>true</c>,
+        /// encryption mode is assumed.</param>
+        /// <returns>Encrypted/Decrypted character corresponding to conditions specified.</returns>
+        private static char GetCharacterSameRow(int currentIndex, PlayfairKey key, bool isDecryption = false)
+        {
+            // Decryption; Character immediately to the left.
+            if (isDecryption)
             {
-                throw new InvalidOperationException(
-                    $"{nameof(characterPosition)} X or Y positions weren't initialised.");
+                // If left-most column: use end column character, otherwise use character to the left of current index.
+                return GetCharColumnIndex(currentIndex) == 0
+                    ? key.Value[currentIndex + (CipherGridDimension - 1)]
+                    : key.Value[currentIndex - 1];
             }
+
+            // Encryption; If character is right-most use first first character within the row, otherwise use
+            // character immediately to the right.
+            return GetCharColumnIndex(currentIndex + 1) == 0
+                ? key.Value[currentIndex - (CipherGridDimension - 1)]
+                : key.Value[currentIndex + 1];
+        }
+
+        /// <summary>
+        /// Based on the current index and whether attempting to encrypt/decrypt appropriate character is retrieved
+        /// from the cipher key value. This method applies when both of the characters are in the same column.
+        /// </summary>
+        /// <param name="currentIndex">Current index of the character attempting to encrypt/decrypt.</param>
+        /// <param name="key">Cipher key being used for encryption/decryption.</param>
+        /// <param name="isDecryption">Flag determining whether to use decryption rule set. If not set to <c>true</c>,
+        /// encryption mode is assumed.</param>
+        /// <returns>Encrypted/Decrypted character corresponding to conditions specified.</returns>
+        private static char GetCharacterSameColumn(int currentIndex, PlayfairKey key, bool isDecryption = false)
+        {
+            // Decryption; Character immediately above.
+            if (isDecryption)
+            {
+                // If top-most row: use bottom row, otherwise use character immediately above. The 4 multiplier makes
+                // it easy to transition from top-most to bottom row within the key.
+                return currentIndex < CipherGridDimension
+                    ? key.Value[(CipherGridDimension * 4) + currentIndex]
+                    : key.Value[currentIndex - CipherGridDimension];
+            }
+
+            // Encryption; If character is on the bottom-most row use character from the top row of the same column,
+            // otherwise use character from the row below.
+            return GetCharRowIndex(currentIndex) == (CipherGridDimension - 1)
+                ? key.Value[currentIndex - (CipherGridDimension * 4)]
+                : key.Value[currentIndex + CipherGridDimension];
+        }
+
+        private static char GetSquareChar(int charOneIndex, int charTwoIndex, PlayfairKey key,
+            bool isDecryption = false, bool isSecondChar = false)
+        {
+            int row;
+
+            if (isDecryption)
+            {
+                if (isSecondChar)
+                {
+                    row = GetCharRowIndex(charTwoIndex) * CipherGridDimension;
+                    return key.Value[row + GetCharColumnIndex(charOneIndex)];
+                }
+
+                row = GetCharRowIndex(charOneIndex) * CipherGridDimension;
+                return key.Value[row + GetCharColumnIndex(charTwoIndex)];
+            }
+
+
+            if (isSecondChar)
+            {
+                row = GetCharRowIndex(charTwoIndex) * CipherGridDimension;
+                return key.Value[row + GetCharColumnIndex(charOneIndex)];
+            }
+
+            // Calculation inside Convert works out the row multiplier and multiplication provides the actual row index.
+            row = GetCharRowIndex(charOneIndex) * CipherGridDimension;
+            return key.Value[row + GetCharColumnIndex(charTwoIndex)];
+        }
+
+        /// <summary>
+        /// Gets characters' row index by utilising its' absolute index value (in relation to cipher key).
+        /// </summary>
+        /// <param name="absoluteIndex">Character index within the cipher key.</param>
+        /// <returns>Row index.</returns>
+        private static int GetCharRowIndex(int absoluteIndex)
+        {
+            return Convert.ToInt32(Math.Floor((double) absoluteIndex / CipherGridDimension));
+        }
+
+        /// <summary>
+        /// Gets characters' column index by utilising its' absolte index value (in relation to cipher key).
+        /// </summary>
+        /// <param name="absoluteIndex">Character index within the cipher key.</param>
+        /// <returns>Column index.</returns>
+        private static int GetCharColumnIndex(int absoluteIndex)
+        {
+            return absoluteIndex % CipherGridDimension;
         }
 
         #endregion
