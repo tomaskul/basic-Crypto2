89dd8adc23e494780fdf4d006cf64b87a7c4396f
Tomas <psytk2@nottingham.ac.uk>
2018-09-22 18:48:46 +0100

    Updating KeyManagement tests to account for changes in previous commit.

--- SimpleCryptographyUnitTests/Cipher Tests/Playfair Cipher/KeyManagementTests.cs
+++ SimpleCryptographyUnitTests/Cipher Tests/Playfair Cipher/KeyManagementTests.cs
@@ -7,79 +7,33 @@ namespace SimpleCryptographyUnitTests.Cipher_Tests.Playfair_Cipher
     public class KeyManagementTests
     {
         private static readonly IPlayfairKeyManagement KeyManagement = new PlayfairKeyManagement();
-        
+
         #region IsValidCipherKey
 
         [Test]
-        [TestCaseSource(nameof(ValidPlayfairCipherKeys))]
-        public void IsValidCipherKey_ValidKey_IsTrue(char[,] cipherKey)
+        [TestCase("ZABCDEFGHIJKLMNOPRSTUVWXY")]
+        [TestCase("GRENOBLIACDFHJKMPSTUVWXYZ")]
+        public void IsValidCipherKey_ValidKey_IsTrue(string cipherKey)
         {
-            Assert.IsTrue(KeyManagement.IsValidCipherKey(cipherKey));
+            Assert.IsTrue(KeyManagement.IsValidCipherKey(new PlayfairKey {Value = cipherKey}));
         }
-        
-        [Test]
-        [TestCaseSource(nameof(InvalidPlayfairCipherKeys))]
-        public void IsValidCipherKey_InvalidKey_IsFalse(char[,] cipherKey)
-        {
-            Assert.IsFalse(KeyManagement.IsValidCipherKey(cipherKey));
-        }
-        
-        private static readonly char[][,] ValidPlayfairCipherKeys =
-        {
-            new char[5,5]
-            {
-                {'Z', 'A', 'B', 'C', 'D'},
-                {'E', 'F', 'G', 'H', 'I'},
-                {'J', 'K', 'L', 'M', 'N'},
-                {'O', 'P', 'R', 'S', 'T'},
-                {'U', 'V', 'W', 'X', 'Y'}
-            },
-            new char[5,5]
-            {
-                {'G', 'R', 'E', 'N', 'O'},
-                {'B', 'L', 'I', 'A', 'C'},
-                {'D', 'F', 'H', 'J', 'K'},
-                {'M', 'P', 'S', 'T', 'U'},
-                {'V', 'W', 'X', 'Y', 'Z'}
-            }, 
-        };
 
-        private static readonly char[][,] InvalidPlayfairCipherKeys =
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("ABC")]
+        [TestCase("abc")]
+        [TestCase("ABCDE")]
+        [TestCase("ABCDEF")]
+        [TestCase("ABCDEFGHIJKLMNOPRSTUVWXYZ")]
+        [TestCase("ABCDEFGHijkLMNOPRSTUVWXYZ")]
+        [TestCase("ABCDEFGHIJKLMNOPQRSTUVWXY")]
+        [TestCase("ZBCDEFGHIJKLMNOPRSTUVWXYA1")]
+        [TestCase("233DEFGHIJKLMNOPRSTUVWXYZ")]
+        public void IsValidCipherKey_InvalidKey_IsFalse(string cipherKey)
         {
-            new char[,]
-            {
-                {'A', 'B', 'C'}
-            },
-            new char[,]
-            {
-                {'A', 'B', 'C', 'D', 'E'}
-            },
-            new char[,]
-            {
-                {'A', 'B', 'C', 'D', 'E', 'F'}
-            },
-            new char[2,3]
-            {
-                {'A', 'B', 'C'},
-                {'D', 'E', 'F'}
-            },
-            new char[5,5]
-            {
-                {'A', 'B', 'C', 'D', 'E'},
-                {'F', 'G', 'H', 'I', 'J'},
-                {'K', 'L', 'M', 'N', 'O'},
-                {'P', 'R', 'S', 'T', 'U'},
-                {'V', 'W', 'X', 'Y', 'Z'}
-            },
-            new char[5,5]
-            {
-                {'A', 'B', 'C', 'D', 'E'},
-                {'F', 'G', 'H', 'I', 'J'},
-                {'K', 'L', 'Q', 'N', 'O'},
-                {'P', 'R', 'S', 'T', 'U'},
-                {'V', 'W', 'X', 'Y', 'Z'}
-            }
-        };
+            Assert.IsFalse(KeyManagement.IsValidCipherKey(new PlayfairKey {Value = cipherKey}));
+        }
 
         #endregion
     }
