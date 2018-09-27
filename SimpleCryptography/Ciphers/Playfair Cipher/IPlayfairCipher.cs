901e02bd1ead6b670eb9effc964d44097553d752
Tomas <psytk2@nottingham.ac.uk>
2018-09-27 22:06:32 +0100

    Removing redundant functions from IPlayfairCipher

--- SimpleCryptography/Ciphers/Playfair Cipher/IPlayfairCipher.cs
+++ SimpleCryptography/Ciphers/Playfair Cipher/IPlayfairCipher.cs
@@ -1,11 +1,12 @@
-﻿using SimpleCryptography.Ciphers.Playfair_Cipher.Key_Management;
+﻿using SimpleCryptography.Ciphers.Generic;
+using SimpleCryptography.Ciphers.Playfair_Cipher.Key_Management;
 
 namespace SimpleCryptography.Ciphers.Playfair_Cipher
 {
     /// <summary>
     /// Interface for Playfair cipher. Read more here: https://en.wikipedia.org/wiki/Playfair_cipher
     /// </summary>
-    public interface IPlayfairCipher
+    public interface IPlayfairCipher : IGenericCipher<PlayfairKey>
     {
         /// <summary>
         /// Encrypts a plain text message by using the specified key.
@@ -14,14 +15,6 @@ namespace SimpleCryptography.Ciphers.Playfair_Cipher
         /// <param name="memorableKey">Memorable key to encrypt text with.</param>
         /// <returns>Encrypted plaintext</returns>
         string EncryptMessage(string plainText, string memorableKey);
-
-        /// <summary>
-        /// Encrypts a plain text message by using a cipher key.
-        /// </summary>
-        /// <param name="plainText">Text to encrypt.</param>
-        /// <param name="cipherKey">Playfair cipher key.</param>
-        /// <returns>Encrypted plaintext</returns>
-        string EncryptMessage(string plainText, PlayfairKey cipherKey);
         
         /// <summary>
         /// Decrypts ciphertext message by using the specified key.
@@ -30,13 +23,5 @@ namespace SimpleCryptography.Ciphers.Playfair_Cipher
         /// <param name="memorableKey">Key to use for decryption.</param>
         /// <returns>Plaintext derived by decrypting ciphertext via supplied key.</returns>
         string DecryptMessage(string cipherText, string memorableKey);
-
-        /// <summary>
-        /// Decypts a ciphertext message by using the specified cipher key.
-        /// </summary>
-        /// <param name="cipherText">Plaintext encrypted via playfair key.</param>
-        /// <param name="cipherKey">Playfair cipher key.</param>
-        /// <returns>Plaintext derived by decrypting ciphertext via supplied key.</returns>
-        string DecryptMessage(string cipherText, PlayfairKey cipherKey);
     }
 }
