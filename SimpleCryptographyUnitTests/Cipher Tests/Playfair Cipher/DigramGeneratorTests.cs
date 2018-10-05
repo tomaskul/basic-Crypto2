92c0c7e2fabbbc02e91247f9095b0a8490addd07
Tomas <psytk2@nottingham.ac.uk>
2018-10-05 13:58:04 +0100

    Updating DigraphGenerator tests to remove no longer used single quation marks

--- SimpleCryptographyUnitTests/Cipher Tests/Playfair Cipher/DigramGeneratorTests.cs
+++ SimpleCryptographyUnitTests/Cipher Tests/Playfair Cipher/DigramGeneratorTests.cs
@@ -9,11 +9,11 @@ namespace SimpleCryptographyUnitTests.Cipher_Tests.Playfair_Cipher
         private static readonly IDigrathGenerator DigrathGenerator = new DigrathGenerator('X');
         
         [Test]
-        [TestCase("X", "'XX'")]
-        [TestCase("LOL", "'LO' 'LX'")]
-        [TestCase("AHEEL", "'AH' 'EX' 'EL'")]
-        [TestCase("NOOO", "'NO' 'OX' 'OX'")]
-        [TestCase("MEETMEATHAMMERSMITHBRIDGETONIGHT", "'ME' 'ET' 'ME' 'AT' 'HA' 'MX' 'ME' 'RS' 'MI' 'TH' 'BR' 'ID' 'GE' 'TO' 'NI' 'GH' 'TX'")]
+        [TestCase("X", "XX")]
+        [TestCase("LOL", "LO LX")]
+        [TestCase("AHEEL", "AH EX EL")]
+        [TestCase("NOOO", "NO OX OX")]
+        [TestCase("MEETMEATHAMMERSMITHBRIDGETONIGHT", "ME ET ME AT HA MX ME RS MI TH BR ID GE TO NI GH TX")]
         public void GenerateDigram_ValidInputs(string message, string toStringOutput)
         {
             DigrathGenerator.GetMessageDigraths(message);
