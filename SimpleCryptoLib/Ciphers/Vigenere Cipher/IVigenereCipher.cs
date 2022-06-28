f6c7c455cc70cee3af3ee4ce38c59834e16d7dbd
tomaskul <>
2022-06-28 17:40:39 +0100

    Xml-docs and formatting improvements.

--- SimpleCryptoLib/Ciphers/Vigenere Cipher/IVigenereCipher.cs
+++ SimpleCryptoLib/Ciphers/Vigenere Cipher/IVigenereCipher.cs
@@ -2,9 +2,7 @@ using SimpleCryptoLib.Ciphers.Common;
 
 namespace SimpleCryptoLib.Ciphers.Vigenere_Cipher;
 
-/// <summary>
-/// Vigenere cipher.
-/// </summary>
+/// <summary>Vigenere cipher.</summary>
 public interface IVigenereCipher : ICommonCipher<VigenereKey>
 {
 }
--- SimpleCryptoLib/Ciphers/Vigenere Cipher/VigenereKey.cs
+++ SimpleCryptoLib/Ciphers/Vigenere Cipher/VigenereKey.cs
@@ -2,10 +2,9 @@ using SimpleCryptoLib.Ciphers.Common.Key_Management;
 
 namespace SimpleCryptoLib.Ciphers.Vigenere_Cipher;
 
+/// <summary>The Vigenere cipher key.</summary>
 public class VigenereKey : CipherKeyBase
 {
-    /// <summary>
-    /// The memorable key.
-    /// </summary>
+    /// <summary>The memorable key.</summary>
     public string MemorableKey { get; set; }
 }
